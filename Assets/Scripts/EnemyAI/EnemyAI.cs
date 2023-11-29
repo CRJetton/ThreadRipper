71ab3bbf4501917f1df8dd125eccce6b79db3205
Tristan Sherzer <TSherzer@student.fullsail.edu>
2023-11-29 13:46:03 -0500

    Enemy AI frame created, will fill it out as we get closer to completion

---
+++ Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehavior
+{
+	// Serialized fileds
+
+	// bools
+
+	// start
+	void Start()
+	{
+
+	}
+
+	// update
+	void Update()
+	{
+
+	}
+
+	// triggers
+	public void OnTriggerEnter(Collider other)
+	{
+
+	}
+
+	// shooting
+	IEnumerator shoot()
+	{
+
+	}
+
+	// taking damage
+	public void takeDamage(int amount)
+	{
+
+	}
+
+	// player feedback from damage
+	IEnumerator flashred()
+	{
+
+	}
+}
