b51e5c821eb7adb542c83729fa1a7d4c850d3eaf
TonyRoldan <134562401+TonyRoldan@users.noreply.github.com>
2023-12-09 02:39:46 -0500

    Audio Folder
    Created an audio folder that has all needed gun sounds. Also has player and UI sounds as well as potential background music and menu music

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -5,27 +5,59 @@ using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
+    public enum ItemType
+    {
+        pistol,
+        rifle,
+        shotgun,
+        smg,
+        sniper,
+
+        smallHealth = 20,
+        medHealth,
+        largeHealth
+    }
+
+    Dictionary<ItemType, string> itemNames = new Dictionary<ItemType, string>()
+    {
+        { ItemType.pistol, "Pistol" },
+        { ItemType.rifle, "Rifle" },
+        { ItemType.shotgun, "Shotgun" },
+        { ItemType.smg, "SMG" },
+        { ItemType.sniper, "Sniper"},
+
+        { ItemType.smallHealth, "Health - S"},
+        { ItemType.medHealth, "Health - M" },
+        { ItemType.largeHealth, "Health - L" }
+    };
+
     public static ItemManager instance { get; private set; }
 
     [SerializeField] float lookDistance;
 
     [SerializeField] Image itemPopup;
+    [SerializeField] Text itemName;
+
+    LayerMask itemPickupMask;
 
     void Awake()
     {
         instance = this;
+        itemPickupMask = LayerMask.GetMask("Pickup");
     }
-
     
-    
-    /*bool LookingAtItem()
+    void PopupItemMenu()
     {
         RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, lookDistance,))
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, lookDistance, itemPickupMask))
         {
             itemPopup.gameObject.SetActive(true);
         }
+        else
+        {
+            itemPopup.gameObject.SetActive(false);
+        }
     }
-    */
+    
    
 }
