fb0ea7e92fe2da592a0561da11042ada61ed39a0
CRJetton <claytonjetton@outlook.com>
2023-12-12 16:18:36 -0500

    Player Interaction Side of Ammo Pickups

---
+++ Assets/Scripts/Environment/AmmoPickUp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickUp : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int ammoAmount;
+
+    public void Interact(IInteractionController _interactController)
+    {
+        //_interactController.PickUpAmmo(this);
+    }
+
+    public int GetAmmoAmount()
+    {
+        return ammoAmount;
+    }
+}
