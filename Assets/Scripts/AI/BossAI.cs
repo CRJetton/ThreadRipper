740fcf3d8a93ef927c6982c324c197c7bfb66ed8
Wolfie <137794343+Wolfskonigs@users.noreply.github.com>
2023-12-12 09:55:18 -0500

    Cleaning up code for the boss and adding my comments

--- Assets/Scripts/AI/BossAI.cs
+++ Assets/Scripts/AI/BossAI.cs
@@ -4,8 +4,10 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
+// Defines the AI behavior for the Boss character in our game.
 public class BossAI: MonoBehaviour, IDamageable
 {
+    // Components attached to the boss character for navigation, rendering, and animation
     [Header("------ Components ------")]
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Renderer model;
@@ -13,6 +15,7 @@ public class BossAI: MonoBehaviour, IDamageable
     [SerializeField] public Animator animator;
     [SerializeField] Collider damageCollider;
 
+    // Configurable stats for the boss character to effect the feel of the boss
     [Header("------ Boss Stats ------")]
     [Range(1, 10000)][SerializeField] private float HP;
     [Range(1, 360)]  [SerializeField] private int viewCone;
@@ -20,10 +23,12 @@ public class BossAI: MonoBehaviour, IDamageable
     [Range(1, 3)]    [SerializeField] private float weakSpotMultiplier;
     [Range(1, 3)]    [SerializeField] private float weakSpotThreshold;
 
+    // Roaming behavior settings for when the boss is not engaging the player
     [SerializeField] int roamDistance;
     [SerializeField] int roamPauseTime;
     [SerializeField] float animationSpeedTransition;
 
+    // Phase speeds to change the boss's movement and turning speed during different phases of the fight
     [Header("------ Phase Speeds ------")]
     [Range(1, 3)]   [SerializeField] private float phaseOneMoveSpeed;
     [Range(1, 3)]   [SerializeField] private float phaseTwoMoveSpeed;
@@ -33,47 +38,56 @@ public class BossAI: MonoBehaviour, IDamageable
     [Range(1, 3)]   [SerializeField] private float phaseTwoTurnSpeed;
     [Range(1, 3)]   [SerializeField] private float phaseThreeTurnSpeed;
 
+    // Attack settings for the boss (can probably be removed)
     [Header("----- Boss Attacks -----")]
     [Range(1, 100)] [SerializeField] private float shootRate;
 
+    // Internal state variables to control AI behavior
     bool istakingdamage;
     bool isShooting = false;
     bool playerInRange;
     bool destinationChosen;
 
+    // Variables to track the position and orientation of the boss relative to the player
     float angleToPlayer;
     float stoppingDistanceOrigional;
     float maxHP;
 
+    // Direction and position tracking
     Vector3 playerDir;
     Vector3 startingPos;
 
+    // Reference to the BossCombat script for combat functionalities
     private BossCombat bossCombat;
 
+    // Initialization of boss's state at the start of the game
     #region initialization
     void Start()
     {
-        // initialization
-        HUDManager.instance.UpdateProgress(1);
-        startingPos = transform.position;
-        stoppingDistanceOrigional = agent.stoppingDistance;
-        bossCombat = GetComponent<BossCombat>();
-        if (bossCombat != null)
+        // initial values and states
+        HUDManager.instance.UpdateProgress(1);                                          // Updates the HUD to show the initial state of the boss
+        startingPos = transform.position;                                               // Stores the boss's starting position for later use
+        stoppingDistanceOrigional = agent.stoppingDistance;                             // Saves the original stopping distance of the navigation agent
+        bossCombat = GetComponent<BossCombat>();                                        // Retrieves the BossCombat component attached to the boss
+        if (bossCombat != null)                                                         // Initializes the max HP with the boss's starting HP for phase changes
         {
             bossCombat.InitializeMaxHP(HP);
         }
     }
     #endregion
 
+    // Update is called once per frame to check and update boss's behavior
     #region update
     void Update()
     {
+        // Update the boss's behavior based on the player's position and the boss's state
         if (agent.isActiveAndEnabled)
         {
+            // Update animation speed based on the boss's movement
             float animatorSpeed = agent.velocity.magnitude;
-
             animator.SetFloat("Speed", Mathf.Lerp(animator.GetFloat("Speed"), animatorSpeed, Time.deltaTime * animationSpeedTransition));
 
+            // Check if the boss should roam or engage with the player
             if (playerInRange && !canSeePlayer() || !Investigate())
             {
                 StartCoroutine(roam());
@@ -86,15 +100,18 @@ public class BossAI: MonoBehaviour, IDamageable
     }
     #endregion
 
+    // Defines the roaming behavior of the boss
     #region boss behavior
     IEnumerator roam()                                                                          
     {
+        // Boss chooses a new destination to roam to if the current destination is reached (change number to change behavior)
         if (agent.remainingDistance < 0.05f && !destinationChosen)
         {
             destinationChosen = true;
             agent.stoppingDistance = 0;
             yield return new WaitForSeconds(roamPauseTime);
 
+            // Selects a random position within its specified radius to roam to
             Vector3 randomPos = Random.insideUnitSphere * roamDistance;
             randomPos += startingPos;
 
@@ -106,17 +123,23 @@ public class BossAI: MonoBehaviour, IDamageable
         }
     }
 
+    // Checks if the boss can see the player based on vision cone and line of sight
     bool canSeePlayer()
     {
+        // Calculate the direction and angle to the player
         playerDir = GameManager.instance.playerBodyPositions.playerCenter.position - headPosition.position;
         angleToPlayer = Vector3.Angle(playerDir, transform.forward);
 
+        // Visualize the line of sight for debugging (can be removed if no longer necesary)
         Debug.DrawRay(headPosition.position, playerDir);
 
+        
         RaycastHit hit;
 
+        // Perform a raycast to detect if the player is within line of sight
         if (Physics.Raycast(headPosition.position, playerDir, out hit))
         {
+            // If the player is detected within the vision cone, engage in combat
             if (hit.collider.CompareTag("Player") && angleToPlayer <= viewCone)
             {
                 agent.SetDestination(GameManager.instance.player.transform.position);
@@ -125,11 +148,13 @@ public class BossAI: MonoBehaviour, IDamageable
                     bossCombat.AimAtPlayer(GameManager.instance.playerBodyPositions.playerCenter.position);
                 }
 
+                // Initiate shooting sequence if the player is within range and not already shooting
                 if (!isShooting)
                 {
                     shoot();
                 }
 
+                // Make the boss face the target if within stopping distance
                 if (agent.remainingDistance < agent.stoppingDistance)
                 {
                     faceTarget();
@@ -138,10 +163,12 @@ public class BossAI: MonoBehaviour, IDamageable
                 return true;
             }
         }
+        // Reset the stopping distance if the player is not in sight
         agent.stoppingDistance = 0;
         return false;
     }
 
+    // Investigate the last known position of the player if the boss is taking damage
     bool Investigate()
     {
         if (istakingdamage)
@@ -154,36 +181,47 @@ public class BossAI: MonoBehaviour, IDamageable
         return false;
     }
 
+    // Method to orient the boss to face the player
     void faceTarget()
     {
+        // Calculate the rotation required to face the player
         Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+
+        // Smoothly rotate towards the player
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * targetFaceSpeed);
     }
 
+    // Used by the animator to adjust the boss's look direction based on the player's position
     void OnAnimatorIK(int layerIndex)
     {
         if (playerInRange)
         {
             Vector3 lookTargetPosition = GameManager.instance.player.transform.position;
 
+            // Set the boss's gaze to follow the player
             animator.SetLookAtWeight(1f); 
             animator.SetLookAtPosition(lookTargetPosition);
         }
     }
 
+    // Triggered when a collider enters the boss's trigger zone
     public void OnTriggerEnter(Collider other)
     {
+        // Check if the player has entered the trigger zone
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
         }
     }
 
+    // Adjusts the boss's speed based on the current combat phase
     public void UpdateSpeed(int phase)
     {
+        // Store base movement and turning speeds
         float baseMoveSpeed = agent.speed;
         float baseTurnSpeed = targetFaceSpeed;
 
+        // Adjust speed based on the phase of the battle
         switch (phase)
         {
             case 1:
@@ -202,47 +240,56 @@ public class BossAI: MonoBehaviour, IDamageable
     }
     #endregion
 
+    // Defines combat behaviors, including shooting at the player
     #region combat
     private void shoot()
     {
+        // Initiate shooting if the boss is not already doing so
         if (!isShooting && bossCombat != null)
         {
             StartCoroutine(ShootSequence());
         }
     }
 
+    // Coroutine to handle the shooting sequence
     private IEnumerator ShootSequence()
     {
 
         isShooting = true;
-        
+
+        // Call the method to fire weapons
         bossCombat.FireWeapons();
-        
+
+        // Wait for a period based on the shooting rate before allowing the next shot (may not be needed)
         yield return new WaitForSeconds(shootRate);
 
         isShooting = false;
 
     }
 
+    // Method to handle the boss taking damage
     public void TakeDamage(float damage)
     {
+        // Calculate the direction and angle to the player for weak spot calculation 
         Vector3 toPlayer = (GameManager.instance.player.transform.position - transform.position).normalized;
         float angleToWeakSpot = Vector3.Angle(-transform.forward, toPlayer);
-        float weakSpotConeAngle = 30f;
+        float weakSpotConeAngle = 30f;                                                      // Change this value to effect the cone angle for extra damage
         if (angleToWeakSpot <= weakSpotConeAngle)
         {
-            damage *= weakSpotMultiplier;
+            damage *= weakSpotMultiplier;                                                   // Weak spot multiplier is changed in unity
         }
 
         HP -= damage;
-        // StartCoroutine(flashRed());
+        // StartCoroutine(flashRed()); (currently commented out because it changes the colors in the scene aswell)
         istakingdamage = true;
 
+        // Changes phase for health threshold
         if (bossCombat != null)
         {
             bossCombat.ChangePhase(HP);
         }
-        
+
+        // Handles the boss death, needs to be changed when an animation ass added for death
         if (HP <= 0)
         {
             Destroy(gameObject);
